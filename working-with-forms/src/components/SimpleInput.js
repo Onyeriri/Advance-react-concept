f2c26a5c074a2e9fa3bc2b0237272676bd07117f
Onyeriri Victor <viktorchinonso@gmail.com>
2023-01-20 23:57:52 +0000

    Storing form inputs using useState hook

--- working-with-forms/src/components/SimpleInput.js
+++ working-with-forms/src/components/SimpleInput.js
@@ -1,9 +1,19 @@
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState('');
+  const handleSubmission = (e) => {
+    e.preventDefault();
+
+    console.log(enteredValue);
+    props.onUserInput(enteredValue);
+    setEnteredValue('')
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmission}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' value={enteredValue} onChange={(e) => setEnteredValue(e.target.value)} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
