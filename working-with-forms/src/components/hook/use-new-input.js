9818da0bcbc9e9a67de0eb5badb81ad8bc99bd2d
Onyeriri Victor <viktorchinonso@gmail.com>
2023-01-23 03:32:31 +0000

    Used useReducer for state management in form validation

--- working-with-forms/src/components/hook/use-new-input.js
+++ working-with-forms/src/components/hook/use-new-input.js
@@ -1,32 +1,74 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const inputReducer = (state, action) => {
+  if (action.type === "CHANGE") {
+    return {
+      enterValue: action.value,
+      isTouched: state.isTouched,
+    };
+  }
+
+  if (action.type === "BLUR") {
+    return {
+      enterValue: state.enterValue,
+      isTouched: true,
+    };
+  }
+
+  if (action.type === "RESET_ENTERED_VALUE") {
+    return {
+      enterValue: "",
+      isTouched: false,
+    };
+  }
+
+  if (action.type === "RESET_ONBLUR") {
+    return {
+      enterValue: "",
+      isTouched: false,
+    };
+  }
+
+  return state;
+};
 
 const useInputValidation = (validateInput) => {
-  const [enterValue, setEnterValue] = useState("");
-  const [isTouched, setIsTouched] = useState(false);
+  // const [enterValue, setEnterValue] = useState("");
+  // const [isTouched, setIsTouched] = useState(false);
 
-  const valueIsValid = validateInput(enterValue);
-  const hasError = !valueIsValid && isTouched;
+  const [inputValue, dispatchInputReducer] = useReducer(inputReducer, {
+    enterValue: "",
+    isTouched: false,
+  });
+
+  const valueIsValid = validateInput(inputValue.enterValue);
+  const hasError = !valueIsValid && inputValue.isTouched;
 
   const inputOnchangeHandler = (event) => {
-    setEnterValue(event.target.value);
+    // setEnterValue(event.target.value);
+    dispatchInputReducer({ type: "CHANGE", value: event.target.value });
   };
 
   const inputOnBlurHandler = (event) => {
-    setIsTouched(true);
+    // setIsTouched(true);
+    dispatchInputReducer({ type: "BLUR", value: false });
   };
 
   const resetInput = () => {
-    setEnterValue("");
-    setIsTouched(false);
+    // setEnterValue("");
+    // setIsTouched(false);
+    dispatchInputReducer({ type: "RESET_ENTERED_VALUE", value: "" });
+    dispatchInputReducer({ type: "RESET_ONBLUR", value: false });
   };
 
   return {
-    value: enterValue,
+    value: inputValue.enterValue,
     hasError,
     resetInput,
     valueIsValid,
     inputOnBlurHandler,
     inputOnchangeHandler,
+    inputValue,
   };
 };
 
